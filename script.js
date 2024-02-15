c00d3a8f5768e9622750ebe272404a95c28ab750
Sepulveda <sepulveeda.guillaume@gmail.com>
2024-02-15 12:43:15 +0100

    add modal start and end and store the name in local storage

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
-console.log('Hello, World!');
+window.onload = () => {
+  document.querySelector("#player-name").focus();
+};
 
-console.log(cards);
+const playerNameInput = document.querySelector("#player-name");
+const startGameButton = document.querySelector("#start-game");
+const modalStart = document.querySelector("#modal-start");
+const playerNameError = document.querySelector("#player-name-error");
+const gameContainer = document.querySelector("#game-container");
+const modalEnd = document.querySelector("#modal-end");
+const playAgainButton = document.querySelector("#play-again");
+const endGameButton = document.querySelector("#end-game");
+
+const playerName = localStorage.getItem("playerName");
+if (playerName) {
+  playerNameInput.value = playerName;
+}
+
+startGameButton.addEventListener("click", startGame);
+
+playerNameInput.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    startGame();
+  }
+});
+
+function startGame() {
+  const playerName = playerNameInput.value.trim();
+
+  if (playerName) {
+    localStorage.setItem("playerName", playerName);
+    modalStart.style.display = "none";
+    playerNameError.style.display = "none";
+    gameContainer.style.display = "flex";
+  } else {
+    playerNameError.style.display = "block";
+  }
+}
+
+endGameButton.addEventListener("click", () => {
+  gameContainer.style.display = "none";
+  modalEnd.style.display = "flex";
+});
+
+playAgainButton.addEventListener("click", () => {
+  modalEnd.style.display = "none";
+  modalStart.style.display = "flex";
+  document.querySelector("#player-name").focus();
+});
