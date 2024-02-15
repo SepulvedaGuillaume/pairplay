7ec677e09cb19bbd6635a28dc6da4b6fb1cccc88
Sepulveda <sepulveeda.guillaume@gmail.com>
2024-02-15 17:45:58 +0100

    api + refacto

--- script.js
+++ script.js
@@ -1,97 +1,139 @@
-window.onload = () => {
-  document.querySelector("#player-name").focus();
+window.onload = initializeGame;
+
+const elements = {
+  playerNameInput: document.querySelector("#player-name"),
+  startGameButton: document.querySelector("#start-game"),
+  modalStart: document.querySelector("#modal-start"),
+  playerNameError: document.querySelector("#player-name-error"),
+  gameContainer: document.querySelector("#game-container"),
+  modalEnd: document.querySelector("#modal-end"),
+  playAgainButton: document.querySelector("#play-again"),
+  topicSelected: document.querySelector("#topics-select"),
 };
 
-const playerNameInput = document.querySelector("#player-name");
-const startGameButton = document.querySelector("#start-game");
-const modalStart = document.querySelector("#modal-start");
-const playerNameError = document.querySelector("#player-name-error");
-const gameContainer = document.querySelector("#game-container");
-const modalEnd = document.querySelector("#modal-end");
-const playAgainButton = document.querySelector("#play-again");
+const localStorageKey = "playerName";
+const clientId = "bfdH9o6tt3oLJSqx04FZILvH3vS0WJQKCmIqJXbxdgo";
+const UNSPLASH_ROOT = "https://api.unsplash.com";
+const countCards = 8;
 
-const playerName = localStorage.getItem("playerName");
-if (playerName) {
-  playerNameInput.value = playerName;
+function initializeGame() {
+  elements.playerNameInput.focus();
+  elements.startGameButton.addEventListener("click", handleStartButtonClick);
+  elements.playerNameInput.addEventListener(
+    "keyup",
+    handlePlayerNameInputKeyup
+  );
+  elements.topicSelected.addEventListener("keyup", handlePlayerNameInputKeyup);
+
+  const playerName = localStorage.getItem(localStorageKey);
+  if (playerName) {
+    elements.playerNameInput.value = playerName;
+  }
+
+  elements.playAgainButton.addEventListener(
+    "click",
+    handlePlayAgainButtonClick
+  );
 }
 
-startGameButton.addEventListener("click", startGame);
+function handleStartButtonClick() {
+  startGame();
+  createCardElements();
+}
 
-playerNameInput.addEventListener("keyup", function (event) {
-  if (event.key === "Enter") {
+function handlePlayerNameInputKeyup(e) {
+  if (e.key === "Enter") {
     startGame();
+    createCardElements();
   }
-});
+}
 
 function startGame() {
-  const playerName = playerNameInput.value.trim();
+  const playerName = elements.playerNameInput.value.trim();
 
   if (playerName) {
-    localStorage.setItem("playerName", playerName);
-    modalStart.style.display = "none";
-    playerNameError.style.display = "none";
-    gameContainer.style.display = "flex";
+    localStorage.setItem(localStorageKey, playerName);
+    elements.modalStart.style.display = "none";
+    elements.playerNameError.style.display = "none";
+    elements.gameContainer.style.display = "flex";
   } else {
-    playerNameError.style.display = "block";
+    elements.playerNameError.style.display = "block";
   }
 }
 
-playAgainButton.addEventListener("click", () => {
-  modalEnd.style.display = "none";
-  modalStart.style.display = "flex";
-  document.querySelector("#player-name").focus();
-});
-
-// Array to store the card images
-const cards = ['card1.jpeg', 'card2.jpeg', 'card3.jpeg', 'card4.jpeg', 'card5.jpeg', 'card6.jpeg', 'card7.jpeg', 'card8.jpeg'];
-
-// Duplicate the cards array
-const combinedCards = [...cards, ...cards];
-
-// Shuffle the combinedCards array
-combinedCards.sort(() => Math.random() - 0.5);
-
-// Get the container element
-const container = document.createElement('div');
-container.classList.add('container');
-
-// Loop through the combinedCards array and create card elements
-combinedCards.forEach((card, index) => {
-    const cardElement = document.createElement('div');
-    cardElement.classList.add('card');
-    
-    const image = document.createElement('img');
-    image.src = `cards/${card}`;
-    
-    cardElement.appendChild(image);
-    container.appendChild(cardElement);
-});
-
-// Append the container to game-container
-gameContainer.appendChild(container);
-
-
-// Function to flip all the cards after 3 seconds
-function flipAllCards() {
-    const cards = document.querySelectorAll('.card');
-    setTimeout(() => {
-        cards.forEach(card => {
-            card.classList.toggle('flipped');
-        });
-    }, 2000); // 2000ms = 2 seconds
+function handlePlayAgainButtonClick() {
+  elements.modalEnd.style.display = "none";
+  elements.modalStart.style.display = "flex";
+  elements.playerNameInput.focus();
 }
 
-// Call the flipAllCards function after the DOM content has loaded
-document.addEventListener('DOMContentLoaded', function() {
-    flipAllCards();
-    
-    const cards = document.querySelectorAll('.card');
-    
-    cards.forEach(card => {
-        card.addEventListener('click', function() {
-            card.classList.toggle('flipped');
-        });
-    });
-});
+async function createCardElements() {
+  try {
+    const unsplashResults = await fetchResults(elements.topicSelected.value);
+    const randomImages = unsplashResults.slice(0, countCards * 2);
 
-console.log(cards);
+    const combinedCards = [...randomImages, ...randomImages];
+    combinedCards.sort(() => Math.random() - 0.5);
+
+    const container = document.createElement("div");
+    container.classList.add("container");
+
+    combinedCards.forEach((card, index) => {
+      const cardElement = document.createElement("div");
+      cardElement.classList.add("card");
+
+      const image = document.createElement("img");
+      image.src = card;
+
+      cardElement.appendChild(image);
+      container.appendChild(cardElement);
+    });
+
+    elements.gameContainer.appendChild(container);
+    flipAllCards();
+    addCardClickListeners();
+  } catch (err) {
+    console.log(err);
+    alert("Failed to search Unsplash");
+  }
+}
+
+const flipAllCards = () => {
+  setTimeout(() => {
+    const cards = document.querySelectorAll(".card");
+    cards.forEach((card) => {
+      card.classList.toggle("flipped");
+    });
+  }, 2000); // 2000ms = 2 seconds
+};
+
+function addCardClickListeners() {
+  const cards = document.querySelectorAll(".card");
+  cards.forEach((card) => {
+    card.addEventListener("click", () => {
+      card.classList.toggle("flipped");
+    });
+  });
+}
+
+async function searchUnsplash(searchQuery) {
+  const endpoint = `${UNSPLASH_ROOT}/photos/random?query='${searchQuery}&client_id=${clientId}&count=${countCards}`;
+  const response = await fetch(endpoint);
+
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+
+  const json = await response.json();
+  return json;
+}
+
+async function fetchResults(searchQuery) {
+  try {
+    const results = await searchUnsplash(searchQuery);
+    return results.map((result) => result.urls.regular);
+  } catch (err) {
+    console.log(err);
+    alert("Failed to search Unsplash");
+  }
+}
